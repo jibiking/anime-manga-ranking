317cc6e238cecf2aef7ad3838b166e6ac5077cd2
jibiking <daiki.jibiki@gmail.com>
2021-09-30 09:29:18 +0900

    初期設定完了

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module AnimeMangaRanking
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.skip_routes true # route.rbを変更しない
+      g.assets false # assets作成しない
+      g.helper false # helper作成しない
+      g.test_framework false # test作成しない
+    end
   end
 end
