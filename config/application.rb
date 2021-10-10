d16c69956b0152989e8a52627c5e662e30a0237b
jibiking <daiki.jibiki@gmail.com>
2021-10-10 18:09:39 +0900

    fix: heroku:application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module AnimeMangaRanking
   class Application < Rails::Application
+
+    config.assets.initialize_on_precompile = false
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
